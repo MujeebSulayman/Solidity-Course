1703dbd1b54fd1d431aa3304cbb1e4ef019b0ce5
MujeebSulayman <sulaymanmujeeb6@gmail.com>
2024-07-10 15:14:22 +0100

    innit commit

--- function.sol
+++ function.sol
@@ -2,11 +2,16 @@
 pragma solidity 0.8.13;
 
 contract LearnFunction {
-
     //uint
     uint256 hey;
 
+    //Get data using function
     function getInfo() public view returns (uint256) {
         return hey;
+    }
+
+    //Updating data using function
+    function updateDate(uint256 _hey) public {
+        hey = _hey;
     }
 }
