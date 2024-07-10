49e17e7392ed189ea83a5484e1558198c04a92b2
MujeebSulayman <sulaymanmujeeb6@gmail.com>
2024-07-10 14:52:29 +0100

    innit commit

---
+++ dataTypes.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+//ARRAY
+contract DataType {
+    // bytes1 public a;
+    // bytes2 public b;
+
+    // bytes1 public a = 0xb5;
+    // bytes1 public b = 0x56;
+
+    //ADDRESS TYPES
+
+    // address public hey;
+    // address public addr = 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB;
+
+    //DEFAULT VALUES
+
+    bool public defaultBool; // false
+    uint256 public number; // 0
+    int256 public defaultInt; // 0
+    address public hey; // 0x00000000
+}
---
+++ function.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+contract LearnFunction {
+
+    //uint
+    uint256 hey;
+
+    function getInfo() public view returns (uint256) {
+        return hey;
+    }
+}
