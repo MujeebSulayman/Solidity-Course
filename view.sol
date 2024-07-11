58f5a078d3e2de717b0707c620d9462f97b539f4
MujeebSulayman <sulaymanmujeeb6@gmail.com>
2024-07-11 15:50:08 +0100

    View

--- view.sol
+++ view.sol
@@ -2,21 +2,21 @@
 pragma solidity 0.8.13;
 
 contract View {
-    // uint256 Num1 = 2;
-    // uint256 Num2 = 9;
+    uint256 public Num1 = 2;
+    uint256 public Num2 = 9;
 
     //Using View to check the variables
     // function getResult() public view returns (uint256, uint256) {
-       
+
     //     return (Num1, Num2);
     // }
 
-    function getResults() public pure returns(uint256 product, uint256 total){
-        uint256 num1 = 11;
-        uint256 num2 = 10;
+    function getResults() public view returns (uint product, uint total) {
+        // uint256 num1 = 11;
+        // uint256 num2 = 10;
 
-        product = num1 * num2;
-        total = num1 + num2;
-
+        product = Num1 * Num2;
+        total = Num1 + Num2;
     }
 }
+ 
