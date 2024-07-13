34925cccc1280dd7e3f79950e9a099c9db70006a
MujeebSulayman <sulaymanmujeeb6@gmail.com>
2024-07-13 08:06:53 +0100

    assert

--- require.sol
+++ require.sol
@@ -1,2 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
+
+//ERROR HANDLING
+//....require Statement
+
+contract Require {
+    function checkInput(uint256 input) public pure returns (string memory) {
+        require(input >= 50, "Invalid uint8");
+        require(input <= 200, "Invalid uint");
+
+    return "Input is uint8";
+    }
+}
